07e9590cf1698e6c8f83004ff40e257b91cc6f86
Kevin Patterson <patter001@gmail.com>
2025-12-07 14:53:26 -0600

    session timeout update

--- qcapi/_client.py
+++ qcapi/_client.py
@@ -3,6 +3,7 @@ import hashlib
 import base64
 from requests import Session, Request
 from pprint import pformat
+from logging import getLogger
 
 from qcapi._object import ObjectEndpoint
 from ._backtests import Backtests
@@ -16,34 +17,42 @@ if TYPE_CHECKING:
 
 T = TypeVar("T")
 
+_LOG = getLogger("qcapi")
 
 class QCClient:
     url: str = ""
     token: str = ""
     backtests: "Backtests"
 
-    def __init__(self, url, user_id, token, *, timeout=60):
+    def __init__(self, url, user_id, token, *, timeout=30):
         self.url = url
         self.user = user_id
         self.token = token
         self._timeout = timeout
-
-        # Get timestamp
-        self._timestamp = str(int(time.time()))
-        time_stamped_token = f"{token}" + ":" + self._timestamp
-        hashed_token = hashlib.sha256(time_stamped_token.encode("utf-8")).hexdigest()
-        authentication = "{}:{}".format(self.user, hashed_token)
-        self._api_token = base64.b64encode(authentication.encode("utf-8")).decode("ascii")
-        self._headers = {
-            "Authorization": f"Basic {self._api_token}",
-            "Timestamp": self._timestamp,
-        }
-        self._session = Session()
-        self._session.headers.update(self._headers)
         self.backtests = Backtests(self, "/backtests")
         self.live = LiveEndpoint(self, "/live")
         self.object = ObjectEndpoint(self, "/object")
         self.compile = CompileEndpoint(self, "/compile")
+
+    @property
+    def _session(self) -> Session:
+        # Get timestamp
+        # not sure of the importance here but we could make this smarter by just redoing the
+        # timestamp code every X seconds
+        # currently the QC timeout appears to be two hours, and if you reuse
+        # the header for more than 2 hours it will fail
+        timestamp = str(int(time.time()))
+        time_stamped_token = f"{self.token}" + ":" + timestamp
+        hashed_token = hashlib.sha256(time_stamped_token.encode("utf-8")).hexdigest()
+        authentication = "{}:{}".format(self.user, hashed_token)
+        api_token = base64.b64encode(authentication.encode("utf-8")).decode("ascii")
+        headers = {
+            "Authorization": f"Basic {api_token}",
+            "Timestamp": timestamp,
+        }
+        session = Session()
+        session.headers.update(headers)
+        return session
 
     @overload
     def request(
@@ -67,11 +76,11 @@ class QCClient:
         prepared_request = self._session.prepare_request(request)
         first_time = time.time()
         while True:
-            response = self._session.send(prepared_request)
+            response = self._session.send(prepared_request, timeout=self._timeout)
             response.raise_for_status()
             resp_data = response.json()
             # if loading, keep polling until we hit the timeout
-            if response.json().get("status", None) == "loading" and time.time() - first_time < self._timeout:
+            if response.json().get("status", None) == "loading" and time.time() - first_time < self._timeout*2:
                 continue
             elif not resp_data.get("success", True):
                 errors = resp_data.get("errors", None)
@@ -79,6 +88,10 @@ class QCClient:
                     error_str = errors[0]
                 else:
                     error_str = "Unknown error"
+                msg = f"QC error for {url}\n\t{error_str}"
+                _LOG.info(msg)
+                _LOG.info("Response data: ")
+                _LOG.info(resp_data)
                 raise QCException(f"QC error for {url}\n\t{error_str}", errors=errors)
             else:
                 break
